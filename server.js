a06620c25aab8ab0fccdd832aa91899665d86e84
Grzegorz Staszczak <grzestamail@gmail.com>
2023-06-10 14:41:42 +0200

    Make server

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const isUser = () => false;
+
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `/views/${name}`));
+  };
+  next();
+});
+
+app.use('/user', (req, res, next) => {
+  if (isUser()) next();
+  else res.send('You need to be an user');
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => {
+  res.show('index.html');
+});
+
+app.get('/about', (req, res) => {
+  res.show('about.html');
+});
+
+app.get('/user/settings', (req, res) => {
+  res.show('forbidden.html');
+});
+
+app.get('/user/panel', (req, res) => {
+  res.show('forbidden.html');
+});
+
+app.use((req, res) => {
+  res.status(404).show('404.html');
+});
+
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
